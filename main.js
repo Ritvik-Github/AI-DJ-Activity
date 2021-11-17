82673b4e30cc5cff422adc739c489b53d91fdcfc
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-11-17 14:28:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// 1kb
+song = "";
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+function setup(){
+    canvas = createCanvas(600,500);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    canvas.center();
+    image(
+        video,
+        0,
+        0,
+        600,
+        500
+    );
+}
+
+function play(){
+    song.play();
+}
